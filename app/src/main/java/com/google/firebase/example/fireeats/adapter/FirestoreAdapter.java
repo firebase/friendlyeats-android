741973dc1ef4aa37312f7ff1837208c98c2bcf44
Sam Stern <samstern@google.com>
2017-10-30 06:25:36 -0700

    Update FirestoreAdapter comments to clarify
    See #4

--- app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java
+++ app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java
@@ -30,6 +30,9 @@ import java.util.ArrayList;
  * Note that this class forgoes some efficiency to gain simplicity. For example, the result of
  * {@link DocumentSnapshot#toObject(Class)} is not cached so the same object may be deserialized
  * many times as the user scrolls.
+ * 
+ * See the adapter classes in FirebaseUI (https://github.com/firebase/FirebaseUI-Android/tree/master/firestore) for a
+ * more efficient implementation of a Firestore RecyclerView Adapter.
  */
 public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
         extends RecyclerView.Adapter<VH> {
