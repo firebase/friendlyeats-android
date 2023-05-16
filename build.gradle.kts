f8552b629f03baab026731ed24c717f34ad768d6
Rosário P. Fernandes <rosariofernandes51@gmail.com>
2023-05-17 00:35:32 +0100

    apply versions plugin at module level

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
-    id("com.github.ben-manes.versions") version "0.41.0" apply false
+    id("com.github.ben-manes.versions") version "0.41.0" apply true
 }
 
 allprojects {
