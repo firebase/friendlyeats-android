0b74641b2ab74fbb9c8c21e8580453697d0e335b
DPEBot <dpebot@google.com>
2023-12-22 04:12:12 -0800

    Auto-update dependencies. (#213)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("androidx.navigation.safeargs") version "2.7.6" apply false
     id("com.github.ben-manes.versions") version "0.50.0" apply true
