003fc639d733a9e1acbb0d67326fe0509b28710a
DPEBot <dpebot@google.com>
2024-11-14 05:58:39 -0500

    Auto-update dependencies. (#249)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
     implementation("androidx.startup:startup-runtime:1.2.0")
 
     // Android architecture components
