87e627385e05a2d2c124c73ecbd2311248a57982
Rosário P. Fernandes <rosariofernandes51@gmail.com>
2026-04-30 04:05:01 +0100

    chore(*): update gradle-versions plugin to 0.54.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.library") version "9.0.0" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("androidx.navigation.safeargs") version "2.9.6" apply false
-    id("com.github.ben-manes.versions") version "0.53.0" apply true
+    id("com.github.ben-manes.versions") version "0.54.0" apply true
 }
 
 allprojects {
